cfca09dfd5e8e2767112fff39998ee8a01bc3d9d
Ahmet Koray Karadağ <koray.2karadag@gmail.com>
2026-08-21 08:36:05 +0300

    Vite + React + Tailwind kurulumu

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Uygulamanın ana bileşeni, Tailwind'in çalıştığını gösteren başlık ekranını render eder
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-purple-600">TODO Uygulaması</h1>
+    </div>
+  )
+}
+
+export default App
